b6d065d783d5868f232d1ac174df43becfde7ffe
red <mmuhamadiev>
2022-03-17 00:42:37 +0500

    update 8 changed onBoarding(improved some performance) android map not working, but other stuff working properly. Move to login function

--- lib/screens/main_menu_page/daily_medication/screens/about_screen.dart
+++ lib/screens/main_menu_page/daily_medication/screens/about_screen.dart
@@ -80,7 +80,6 @@ class AboutScreen extends StatelessWidget {
                           },
                         ),
                       ),
-
                     ],
                   ),
           ),
